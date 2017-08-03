819953faaf829eb03f9c9220b7d8a4016e53378b
Thomas Pfennig <thomas.pfennig@simplesystem.com>
2017-08-03 17:27:11 +0200

    fix path browser separator; add teamcity output

--- index.js
+++ index.js
@@ -1,16 +1,80 @@
 var fs = require('fs');
 var path = require('path');
 var reporter = require('./reporter.js');
+var UNDEFINED;
 
 var hasOwnProperty = Object.prototype.hasOwnProperty;
 
-const __featureDenominator = 'Feature: ';
-const __scenarioDenominator = ' - Scenario: ';
+
+const __featureDenominator = 'Feature ';
+const __scenarioDenominator = ' Scenario ';
 
 function HTMLScreenshotReporter(options) {
 
+    options = options || {};
     var self = this;
+    self.started = false;
+    self.finished = false;
     self.tsStart = new Date();
+
+
+    function isFailed(obj) { return obj.status === "failed"; }
+    function isSkipped(obj) { return obj.status === "pending"; }
+    function isDisabled(obj) { return obj.status === "disabled"; }
+    function pad(n) { return n < 10 ? '0'+n : n; }
+    function padThree(n) { return n < 10 ? '00'+n : n < 100 ? '0'+n : n; }
+    function extend(dupe, obj) { // performs a shallow copy of all props of `obj` onto `dupe`
+        for (var prop in obj) {
+            if (obj.hasOwnProperty(prop)) {
+                dupe[prop] = obj[prop];
+            }
+        }
+        return dupe;
+    }
+    function ISODateString(d) {
+        return d.getUTCFullYear() + '-' +
+            pad(d.getUTCMonth()+1) + '-' +
+            pad(d.getUTCDate()) + 'T' +
+            pad(d.getUTCHours()) + ':' +
+            pad(d.getUTCMinutes()) + ':' +
+            pad(d.getUTCSeconds()) + '.' +
+            // TeamCity wants ss.SSS
+            padThree(d.getUTCMilliseconds());
+    }
+    function log(str) {
+        var con = global.console || console;
+        if (con && con.log) {
+            con.log(str);
+        }
+    }
+
+    if(options.modifySuiteName && typeof options.modifySuiteName !== 'function') {
+        throw new Error('option "modifySuiteName" must be a function');
+    }
+
+    var delegates = {};
+    delegates.modifySuiteName = options.modifySuiteName;
+
+    var currentSuite = null,
+        totalSpecsDefined,
+        // when use use fit, jasmine never calls suiteStarted / suiteDone, so make a fake one to use
+        fakeFocusedSuite = {
+            id: 'focused',
+            description: 'focused specs',
+            fullName: 'focused specs'
+        };
+
+    var __suites = {}, __specs = {};
+    function getSuite(suite) {
+        __suites[suite.id] = extend(__suites[suite.id] || {}, suite);
+        return __suites[suite.id];
+    }
+    function getSpec(spec) {
+        __specs[spec.id] = extend(__specs[spec.id] || {}, spec);
+        return __specs[spec.id];
+    }
+
+
 
     options.title = options.title || 'Protractor End to End Test Report';
     options.screenshotsFolder = options.screenshotsFolder || 'screenshots';
@@ -18,12 +82,31 @@ function HTMLScreenshotReporter(options) {
     options.targetPath = options.targetPath || 'target';
 
     self.jasmineStarted = function (summary) {
+        totalSpecsDefined = summary && summary.totalSpecsDefined || NaN;
+        self.started = true;
+        tclog("progressStart 'Running Tests'");
     };
 
     self.suiteStarted = function (suite) {
+        suite = getSuite(suite);
+        suite._parent = currentSuite;
+        currentSuite = suite;
+        tclog("testSuiteStarted", {
+            name: suite.description
+        });
     };
 
     self.specStarted = function (spec) {
+        if (!currentSuite) {
+            // focused spec (fit) -- suiteStarted was never called
+            self.suiteStarted(fakeFocusedSuite);
+        }
+        tcSpec = getSpec(spec);
+        tclog("testStarted", {
+            name: tcSpec.description,
+            captureStandardOutput: 'true'
+        });
+
         var featureName = spec.fullName.replace(spec.description, '');
         spec.description = __featureDenominator + featureName + __scenarioDenominator + spec.description;
         browser.currentTest = spec.description;
@@ -36,14 +119,51 @@ function HTMLScreenshotReporter(options) {
 
     self.specDone = function (spec) {
         browser.takeScreenshot().then(function (png) {
-            writeScreenShot(png, path.join(options.targetPath, options.screenshotsFolder, "-", sanitizeFilename(spec.description)) + '.png');
+            writeScreenShot(png, path.join(options.targetPath, options.screenshotsFolder, sanitizeFilename(spec.description)) + '.png');
         });
+        tcSpec = getSpec(spec);
+        if (isSkipped(spec) || isDisabled(spec)) {
+            tclog("testIgnored", {
+                name: tcSpec.description
+            });
+        }
+        // TeamCity specifies there should only be a single `testFailed`
+        // message, so we'll only grab the first failedExpectation
+        if (isFailed(spec) && tcSpec.failedExpectations.length) {
+            var failure = tcSpec.failedExpectations[0];
+            tclog("testFailed", {
+                name: tcSpec.description,
+                message: failure.message,
+                details: failure.stack
+            });
+        }
+        tclog("testFinished", {
+            name: tcSpec.description
+        });
+
+
     };
 
     self.suiteDone = function (suite) {
+        suite = getSuite(suite);
+        if (suite._parent === UNDEFINED) {
+            // disabled suite (xdescribe) -- suiteStarted was never called
+            self.suiteStarted(suite);
+        }
+        tclog("testSuiteFinished", {
+            name: suite.description
+        });
+        currentSuite = suite._parent;
     };
 
     self.jasmineDone = function () {
+        if (currentSuite) {
+            // focused spec (fit) -- suiteDone was never called
+            self.suiteDone(fakeFocusedSuite);
+        }
+        tclog("progressFinish 'Running Tests'");
+
+        self.finished = true;
     };
 
     self.generateHtmlReport = function (inputFile) {
@@ -60,7 +180,7 @@ function HTMLScreenshotReporter(options) {
 
     var sanitizeFilename = function (name) {
         name = name.replace(/\s+/g, '-'); // Replace white space with dash
-        return name.replace(/[^0-9a-zA-Z\-]/gi, ''); // Strip any special characters except the dash
+        return name.replace(/[^0-9a-zA-Z\-|]/gi, ''); // Strip any special characters except the dash
     };
 
     function generateReport(jsonstr, automationHeader, elapsedTime) {
@@ -260,7 +380,7 @@ function HTMLScreenshotReporter(options) {
         for (var i = 0; i < sortedBrowsers.length; i++) {
             result += '<th>' + sortedBrowsers[i] + '</th>';
         }
-        result += '</tr>'
+        result += '</tr>';
         return result;
     }
 
@@ -280,7 +400,7 @@ function HTMLScreenshotReporter(options) {
     }
 
     function runId(scenarioName, browserName) {
-        return sanitizeFilename(scenarioName) + sanitizeFilename(browserName);
+        return sanitizeFilename(scenarioName) + "|"+  sanitizeFilename(browserName);
     }
 
     function copyResultsToFeatureCollection(resultArray) {
@@ -527,7 +647,7 @@ function HTMLScreenshotReporter(options) {
         }
         for (var i = 0; i < tempFiles.length; i++) {
             var bugPath = path.join(path.resolve(options.targetPath), tempFiles[i]);
-            var raw = fs.readFileSync(bugPath)
+            var raw = fs.readFileSync(bugPath);
             var bug = JSON.parse(raw);
             var key = bug.key;
             var type = '<img src="' + bug.fields.issuetype.iconUrl + '" title="' + bug.fields.issuetype.description + '">';
@@ -562,6 +682,45 @@ function HTMLScreenshotReporter(options) {
         str += seconds + ' secs.';
         return str;
     }
+
+    function tclog(message, attrs) {
+        var str = "##teamcity[" + message;
+        if (typeof(attrs) === "object") {
+            if (!("timestamp" in attrs)) {
+                attrs.timestamp = new Date();
+            }
+            for (var prop in attrs) {
+                if (attrs.hasOwnProperty(prop)) {
+                    if(delegates.modifySuiteName && message.indexOf('testSuite') === 0 && prop === 'name') {
+                        attrs[prop] = delegates.modifySuiteName(attrs[prop]);
+                    }
+                    str += " " + prop + "='" + escapeTeamCityString(attrs[prop]) + "'";
+                }
+            }
+        }
+        str += "]";
+        log(str);
+    }
+
+    function escapeTeamCityString(str) {
+        if(!str) {
+            return "";
+        }
+        if (Object.prototype.toString.call(str) === '[object Date]') {
+            return ISODateString(str);
+        }
+
+        return str.replace(/\|/g, "||")
+            .replace(/\'/g, "|'")
+            .replace(/\n/g, "|n")
+            .replace(/\r/g, "|r")
+            .replace(/\u0085/g, "|x")
+            .replace(/\u2028/g, "|l")
+            .replace(/\u2029/g, "|p")
+            .replace(/\[/g, "|[")
+            .replace(/]/g, "|]");
+    }
+
 
     return this;
 }
